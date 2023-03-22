14cb497ffd144f2b639b16476f2f41919765ae7a
fredy887 <fredy887>
2023-03-22 13:30:34 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	it l;
+	int l;
 	l = n % 10;
 	if (l < 0)
 	{
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,4 +20,4 @@ void times_table(void)
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
-		_putchar("\n");
+		_putchar('\n');
